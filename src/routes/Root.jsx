f199320c7bc5a8a2a4761714759abab7caa2c3c2
Yengzzkie <gatchalian.manuel@gmail.com>
2024-06-03 15:09:41 -0400

    removed .env file

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -30,12 +30,11 @@ export default function Root() {
   const [recentlyViewed, setRecentlyViewed] = useState(() => {return JSON.parse(localStorage.getItem("recentlyViewed")) || []});
   const [continueWatch, setContinueWatch] = useState(() => {return JSON.parse(localStorage.getItem("continueWatch")) || []});
 
-  const bearerToken = import.meta.env.VITE_BEARER;
   const options = {
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization: `${bearerToken}`,
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOTkwMGIwZmVhZjZjZjVkMjk0MDc1YjAxNDRkMmZiYSIsInN1YiI6IjY2MTA4NDQ1NWIzNzBkMDE3MDYzMzFjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.r2V8Oru9xaAu4JLQPZw_nqv_lVULwa-ZPfq8ruQHwvg",
     },
   };
 
